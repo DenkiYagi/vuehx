4ab197790c9593bfc79e6da0d1ce776ebcb24a8c
terurou <terurou@denkiyagi.jp>
2018-03-14 15:44:06 +0900

    Remove dead code

--- src/vuehx/Vue.hx
+++ src/vuehx/Vue.hx
@@ -504,24 +504,3 @@ typedef WatchOptions = {
 }
 
 typedef Unwatch = Void -> Void;
-
-class VueHelper {
-    public static macro function vue() {
-        var cls = Context.getLocalClass().get();
-        for (s in cls.statics.get()) {
-            if (s.name != "options") continue;
-
-            switch (s.type) {
-                case TType(_, params):
-                    var T = Context.toComplexType(params[0]);
-                    return macro {
-                        var vue: vuehx.Vue<$T> = js.Lib.nativeThis;
-                        vue;
-                    }
-                case _:
-                    break;
-            }
-        }
-        return macro null;
-    }
-}
