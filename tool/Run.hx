7111f2edd682d2147e161e19cbdf7b0409371ba8
terurou <terurou@denkiyagi.jp>
2018-03-26 23:30:46 +0900

    Add project template setup

--- tool/Run.hx
+++ tool/Run.hx
@@ -3,17 +3,23 @@ package;
 import sys.FileSystem;
 import sys.io.File;
 
+/**
+ * haxelib run command
+ */
 class Run {
+    static var PATH_SEPARATOR = if (Sys.systemName() == "Windows") "\\" else "/";
+
     public static function main() {
         var arg = readArgs();
         switch (arg.command) {
             case "setup":
-                setupWithNpm(arg.dir);
+                setup(arg.dir, false);
             case "setup-yarn":
-                setupWithYarn(arg.dir);
+                setup(arg.dir, true);
             case _:
                 printUsage();
         }
+        Sys.exit(0);
     }
 
     static function readArgs(): { command: Null<String>, dir: Null<String> } {
@@ -21,7 +27,7 @@ class Run {
         var args = Sys.args();
         if (args.length == 2) {
             if (FileSystem.isDirectory(args[1])) {
-                return { command: args[0], dir: args[1] }
+                return { command: args[0], dir: args[1] };
             }
         }
         return { command: null, dir: null };
@@ -31,22 +37,52 @@ class Run {
         Sys.println("usage: haxelib run hxgnd <command>");
         Sys.println("  setup        setup with npm");
         Sys.println("  setup-yarn   setup with yarn");
-        Sys.exit(0);
     }
 
-    static function setupWithNpm(dir: String) {
-        Sys.setCwd(dir);
-        Sys.command('npm i -D ${getDependencies().join(" ")}');
-        Sys.exit(0);
+    static function setup(projectDir: String, yarnEnabled: Bool) {
+        var libDir = getDirectory(Sys.programPath());
+        var templateDir = joinPath([libDir, "tool", "template"]);
+        copy(templateDir, projectDir);
+
+        Sys.setCwd(projectDir);
+        if (yarnEnabled) {
+            Sys.command('npm i -D ${getDependencies()}');
+        } else {
+            Sys.command('yarn add -D ${getDependencies()}');
+        }
     }
 
-    static function setupWithYarn(dir: String) {
-        Sys.setCwd(dir);
-        Sys.command('yarn add -D ${getDependencies().join(" ")}');
-        Sys.exit(0);
+    static inline function getDirectory(path: String): String {
+        return new EReg('\\${PATH_SEPARATOR}[^\\${PATH_SEPARATOR}]+$', "").replace(path, "");
     }
 
-    static function getDependencies() {
+    static inline function joinPath(route: Array<String>): String {
+        return route.join(PATH_SEPARATOR);
+    }
+
+    static function copy(srcDir: String, distDir: String): Void {
+        if (!FileSystem.isDirectory(srcDir)) throw '${srcDir} is not directory';
+        if (!FileSystem.isDirectory(distDir)) throw '${distDir} is not directory';
+        if (FileSystem.readDirectory(distDir).length > 0) throw '${distDir} is not empty directory';
+
+        var stack = [];
+        stack.push("");
+        while (stack.length > 0) {
+            var node = stack.pop();
+            var srcPath = joinPath([srcDir, node]);
+            var distPath = joinPath([distDir, node]);
+            if (FileSystem.isDirectory(srcPath)) {
+                for (child in FileSystem.readDirectory(srcPath)) {
+                    stack.push(joinPath([node, child]));
+                }
+                FileSystem.createDirectory(distPath);
+            } else {
+                File.copy(srcPath, distPath);
+            }
+        }
+    }
+
+    static function getDependencies(): String {
         var jsonPath = ~/run.n$/.replace(Sys.programPath(), "package.json");
         var content = File.getContent(jsonPath);
         var dependencies: Dynamic<String> = haxe.Json.parse(content).devDependencies;
@@ -55,6 +91,6 @@ class Run {
         for (name in Reflect.fields(dependencies)) {
             result.push('$name@"${Reflect.field(dependencies, name)}"');
         }
-        return result;
+        return result.join(" ");
     }
 }
