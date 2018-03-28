5065809e9c3c712c771f58c56acb42cce7df2abc
terurou <terurou@denkiyagi.jp>
2018-03-28 23:31:16 +0900

    Update project-template

--- tool/template/src/Main.hx
+++ tool/template/src/Main.hx
@@ -4,6 +4,11 @@ import vuehx.Vue;
 
 class Main {
     public static function main(): Void {
-
+        var vue = new Vue({
+            render: function (createElement) {
+                return createElement(App);
+            }
+        });
+        vue.mount("#app");
     }
 }
