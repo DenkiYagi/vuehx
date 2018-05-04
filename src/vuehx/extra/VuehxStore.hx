443adeb0ad9ebed583aa59927713760b54859ee9
terurou <terurou@denkiyagi.jp>
2018-05-04 19:21:04 +0900

    Add some test cases of VuehxStore

--- src/vuehx/extra/VuehxStore.hx
+++ src/vuehx/extra/VuehxStore.hx
@@ -6,39 +6,49 @@ import hxgnd.LangTools;
 import externtype.ValueOrArray;
 
 class VuehxStore<TState, TAction> {
-    var currentState: TState;
+    public var state(default, null): TState;
     var subscribers: Array<Subscriber<TState>>;
     var middlewares: Array<Middleware<TState, TAction>>;
 
     public function new(initState: TState, middlewares: ValueOrArray<Middleware<TState, TAction>>) {
-        this.currentState = #if debug LangTools.freeze(initState) #else initState #end;
+        this.state = #if debug LangTools.freeze(initState) #else initState #end;
         this.subscribers = [];
-        this.middlewares = middlewares.toArray().copy();
+        this.middlewares = middlewares.toArray();
     }
 
     public function dispatch(action: TAction): Future<Unit> {
-        var i = 0;
         var len = middlewares.length;
+        if (len <= 0) return Future.successfulUnit();
+
+        var i = 0;
+        var completed = false;
+
+        function commit(reducer: TState -> TState): Void {
+            if (completed) return;
+
+            var newState = reducer(state);
+            if (LangTools.notSame(state, newState)) {
+                state = #if debug LangTools.freeze(newState) #else state #end;
+                for (f in subscribers) f(state);
+            }
+        }
+
         function next() {
             return if (i < len) {
-                middlewares[i++]({
-                    state: currentState,
+                var future = middlewares[i++]({
+                    state: state,
                     action: action,
                     commit: commit,
                 }, next);
+                future.then(function (_) {
+                    completed = true;
+                });
+                future;
             } else {
                 Future.successfulUnit();
             }
         }
         return next();
-    }
-
-    function commit(reducer: TState -> TState): Void {
-        var state = reducer(currentState);
-        if (LangTools.notSame(currentState, state)) {
-            currentState = #if debug LangTools.freeze(state) #else state #end;
-            for (f in subscribers) f(currentState);
-        }
     }
 
     public function subscribe(fn: Subscriber<TState>): Void {
