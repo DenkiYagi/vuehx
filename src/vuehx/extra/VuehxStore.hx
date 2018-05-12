e9bbf600b9c52ff3b14ba72836e19b1c42c525c6
terurou <terurou@denkiyagi.jp>
2018-05-12 13:47:59 +0900

    Remove multiple middleware feature

--- src/vuehx/extra/VuehxStore.hx
+++ src/vuehx/extra/VuehxStore.hx
@@ -3,53 +3,32 @@ package vuehx.extra;
 import hxgnd.Unit;
 import hxgnd.Future;
 import hxgnd.LangTools;
-import externtype.ValueOrArray;
 
 class VuehxStore<TState, TAction> {
     public var state(default, null): TState;
     var subscribers: Array<Subscriber<TState>>;
-    var middlewares: Array<Middleware<TState, TAction>>;
+    var middleware: Middleware<TState, TAction>;
 
-    public function new(initState: TState, middlewares: ValueOrArray<Middleware<TState, TAction>>) {
+    public function new(initState: TState, middleware: Middleware<TState, TAction>) {
         this.state = #if debug LangTools.freeze(initState) #else initState #end;
         this.subscribers = [];
-        this.middlewares = middlewares.toArray();
+        this.middleware = middleware;
     }
 
     public function dispatch(action: TAction): Future<Unit> {
-        var len = middlewares.length;
-        if (len <= 0) return Future.successfulUnit();
+        return middleware({
+            state: state,
+            action: action,
+            commit: commit,
+        });
+    }
 
-        var i = 0;
-        var completed = false;
-
-        function commit(reducer: TState -> TState): Void {
-            if (completed) return;
-
-            var newState = reducer(state);
-            if (LangTools.notSame(state, newState)) {
-                state = #if debug LangTools.freeze(newState) #else state #end;
-                for (f in subscribers) f(state);
-            }
+    function commit(reducer: TState -> TState): Void {
+        var newState = reducer(state);
+        if (LangTools.notSame(state, newState)) {
+            state = #if debug LangTools.freeze(newState) #else state #end;
+            for (f in subscribers) f(state);
         }
-
-        function next() {
-            return if (i < len) {
-                var future = middlewares[i++]({
-                    state: state,
-                    action: action,
-                    commit: commit,
-                    next: next,
-                });
-                future.then(function (_) {
-                    completed = true;
-                });
-                future;
-            } else {
-                Future.successfulUnit();
-            }
-        }
-        return next();
     }
 
     public function subscribe(fn: Subscriber<TState>): Void {
@@ -67,7 +46,6 @@ typedef Context<TState, TAction> = {
     var state(default, null): TState;
     var action(default, null): TAction;
     function commit(reducer: TState -> TState): Void;
-    function next(): Future<Unit>;
 }
 
 typedef Subscriber<TState> = TState -> Void;
--- test/vuehx/extra/VuehxStoreTest.hx
+++ test/vuehx/extra/VuehxStoreTest.hx
@@ -14,239 +14,146 @@ class VuehxStoreTest extends BuddySuite {
         describe("VuehxStore.dispatch()", {
             timeoutMs = 100;
 
-            describe("No middleware", {
-                it("should not throw error", function (done) {
-                    var store = new VuehxStore(0, []);
-                    store.dispatch(Increment);
-                    Timer.delay(function () {
-                        done();
-                    }, 10);
+            it("should call", function (done) {
+                var called = false;
+                var store = new VuehxStore(0, function (ctx) {
+                    called = true;
+                    ctx.commit(function (state) return state);
+                    return Future.successfulUnit();
+                });
+                store.dispatch(Increment);
+                Timer.delay(function () {
+                    called.should.be(true);
+                    done();
+                }, 10);
+            });
+
+            it("should call 2-times", function (done) {
+                var called = 0;
+                var store = new VuehxStore(0, function (ctx) {
+                    called++;
+                    return Future.successfulUnit();
+                });
+                store.dispatch(Increment);
+                store.dispatch(Increment);
+
+                Timer.delay(function () {
+                    called.should.be(2);
+                    done();
+                }, 10);
+            });
+
+            it("should not notify when it has given same state", function (done) {
+                var store = new VuehxStore({data: 1}, function (ctx) {
+                    ctx.commit(function (state) return {data: 1});
+                    return Future.successfulUnit();
+                });
+                store.subscribe(function (x) {
+                    fail();
+                    done();
+                });
+                store.dispatch(Increment);
+                Timer.delay(done, 10);
+            });
+
+            it("should notify when it has given different state", function (done) {
+                var store = new VuehxStore({data: 1}, function (ctx) {
+                    ctx.commit(function (state) return {data: 2});
+                    return Future.successfulUnit();
+                });
+                store.subscribe(function (x) {
+                    x.same({data: 2}).should.be(true);
+                    done();
+                });
+                store.dispatch(Increment);
+            });
+
+            it("should notify 2-times", function (done) {
+                var store = new VuehxStore(0, function (ctx) {
+                    ctx.commit(function (state) return state + 1);
+                    return Future.successfulUnit();
                 });
 
+                var count = 0;
+                store.subscribe(function (x) {
+                    switch (count) {
+                        case 0:
+                            x.same(1).should.be(true);
+                        case 1:
+                            x.same(2).should.be(true);
+                        case _:
+                            fail();
+                            done();
+                    }
+                });
+
+                store.dispatch(Increment);
+                count++;
+                store.dispatch(Increment);
+                count++;
+                Timer.delay(done, 10);
+            });
+
+            it("should notify 2-subscribers", function (done) {
+                var store = new VuehxStore(0, function (ctx) {
+                    ctx.commit(function (state) return state + 1);
+                    return Future.successfulUnit();
+                });
+
+                var count1 = 0;
+                store.subscribe(function (x) {
+                    x.should.be(++count1);
+                });
+
+                var count2 = 0;
+                store.subscribe(function (x) {
+                    x.should.be(++count2);
+                });
+
+                store.dispatch(Increment);
+                store.dispatch(Increment);
+                Timer.delay(function () {
+                    count1.should.be(2);
+                    count2.should.be(2);
+                    done();
+                }, 10);
+            });
+
+            describe("returned Future", {
                 it("should not be active", {
-                    var store = new VuehxStore({data: 1}, []);
+                    var store = new VuehxStore({data: 1}, function (ctx) {
+                        return Future.successfulUnit();
+                    });
                     var future = store.dispatch(Increment);
                     future.isActive.should.be(false);
                 });
-            });
 
-            describe("Single middleware", {
-                it("should call", function (done) {
-                    var called = false;
-                    var store = new VuehxStore(0, function (ctx) {
-                        called = true;
-                        ctx.commit(function (state) return state);
-                        return Future.successfulUnit();
-                    });
-                    store.dispatch(Increment);
-                    Timer.delay(function () {
-                        called.should.be(true);
-                        done();
-                    }, 10);
-                });
-
-                it("should call 2-times", function (done) {
-                    var called = 0;
-                    var store = new VuehxStore(0, function (ctx) {
-                        called++;
-                        return Future.successfulUnit();
-                    });
-                    store.dispatch(Increment);
-                    store.dispatch(Increment);
-
-                    Timer.delay(function () {
-                        called.should.be(2);
-                        done();
-                    }, 10);
-                });
-
-                it("should not notify when it call next", function (done) {
-                    var store = new VuehxStore(0, function (ctx) {
-                        return ctx.next();
-                    });
-                    store.subscribe(function (x) {
-                        fail();
-                        done();
-                    });
-                    store.dispatch(Increment);
-                    Timer.delay(done, 10);
-                });
-
-                it("should not notify when it has given same state", function (done) {
+                it("should abort with Future", function (done) {
                     var store = new VuehxStore({data: 1}, function (ctx) {
-                        ctx.commit(function (state) return {data: 1});
-                        return Future.successfulUnit();
-                    });
-                    store.subscribe(function (x) {
-                        fail();
-                        done();
-                    });
-                    store.dispatch(Increment);
-                    Timer.delay(done, 10);
-                });
-
-                it("should notify when it has given different state", function (done) {
-                    var store = new VuehxStore({data: 1}, function (ctx) {
-                        ctx.commit(function (state) return {data: 2});
-                        return Future.successfulUnit();
-                    });
-                    store.subscribe(function (x) {
-                        x.same({data: 2}).should.be(true);
-                        done();
-                    });
-                    store.dispatch(Increment);
-                });
-
-                it("should notify 2-times", function (done) {
-                    var store = new VuehxStore(0, function (ctx) {
-                        ctx.commit(function (state) return state + 1);
-                        return Future.successfulUnit();
-                    });
-
-                    var count = 0;
-                    store.subscribe(function (x) {
-                        switch (count) {
-                            case 0:
-                                x.same(1).should.be(true);
-                            case 1:
-                                x.same(2).should.be(true);
-                            case _:
-                                fail();
+                        return Future.applySync(function (ctx) {
+                            ctx.onAbort = function () {
                                 done();
-                        }
+                            }
+                        });
                     });
-
-                    store.dispatch(Increment);
-                    count++;
-                    store.dispatch(Increment);
-                    count++;
-                    Timer.delay(done, 10);
+                    var future = store.dispatch(Increment);
+                    future.abort();
                 });
 
-                it("should notify 2-subscribers", function (done) {
-                    var store = new VuehxStore(0, function (ctx) {
-                        ctx.commit(function (state) return state + 1);
-                        return Future.successfulUnit();
+                it("should abort with Stream", function (done) {
+                    var store = new VuehxStore({data: 1}, function (_) {
+                        return Stream.apply(function (ctx) {
+                            ctx.onAbort = function () {
+                                done();
+                            }
+                        }).end;
                     });
 
-                    var count1 = 0;
-                    store.subscribe(function (x) {
-                        x.should.be(++count1);
-                    });
-
-                    var count2 = 0;
-                    store.subscribe(function (x) {
-                        x.should.be(++count2);
-                    });
-
-                    store.dispatch(Increment);
-                    store.dispatch(Increment);
+                    var future = store.dispatch(Increment);
                     Timer.delay(function () {
-                        count1.should.be(2);
-                        count2.should.be(2);
-                        done();
-                    }, 10);
-                });
-
-                describe("returned Future", {
-                    it("should not be active", {
-                        var store = new VuehxStore({data: 1}, function (ctx) {
-                            return Future.successfulUnit();
-                        });
-                        var future = store.dispatch(Increment);
-                        future.isActive.should.be(false);
-                    });
-
-                    it("should abort with Future", function (done) {
-                        var store = new VuehxStore({data: 1}, function (ctx) {
-                            return Future.applySync(function (ctx) {
-                                ctx.onAbort = function () {
-                                    done();
-                                }
-                            });
-                        });
-                        var future = store.dispatch(Increment);
                         future.abort();
-                    });
-
-                    it("should abort with Stream", function (done) {
-                        var store = new VuehxStore({data: 1}, function (_) {
-                            return Stream.apply(function (ctx) {
-                                ctx.onAbort = function () {
-                                    done();
-                                }
-                            }).end;
-                        });
-
-                        var future = store.dispatch(Increment);
-                        Timer.delay(function () {
-                            future.abort();
-                        }, 10);
-                    });
-                });
-            });
-
-            describe("Multiple middleware", {
-                it("should call only 1st middleware", function (done) {
-                    var called = false;
-                    var store = new VuehxStore(0, [
-                        function (ctx) {
-                            called = true;
-                            return Future.successfulUnit();
-                        },
-                        function (ctx) {
-                            fail();
-                            return Future.successfulUnit();
-                        }
-                    ]);
-                    store.dispatch(Increment);
-                    Timer.delay(function () {
-                        called.should.be(true);
-                        done();
                     }, 10);
                 });
-
-                it("should call 2nd middleware", function (done) {
-                    var called1 = false;
-                    var called2 = false;
-                    var store = new VuehxStore(0, [
-                        function (ctx) {
-                            called1 = true;
-                            return ctx.next();
-                        },
-                        function (ctx) {
-                            called2 = true;
-                            return Future.successfulUnit();
-                        }
-                    ]);
-                    store.dispatch(Increment);
-                    Timer.delay(function () {
-                        called1.should.be(true);
-                        called2.should.be(true);
-                        done();
-                    }, 10);
-                });
-
-                it("should not commit when it already completed", function (done) {
-                    var store = new VuehxStore(0, [
-                        function (ctx) {
-                            Timer.delay(function () {
-                                ctx.commit(function (x) return 1);
-                            }, 5);
-                            return ctx.next();
-                        },
-                        function (ctx) {
-                            ctx.commit(function (x) return 2);
-                            return Future.successfulUnit();
-                        }
-                    ]);
-                    store.dispatch(Increment);
-                    Timer.delay(function () {
-                        store.state.should.be(2);
-                        done();
-                    }, 10);
-                });
-
             });
         });
 
